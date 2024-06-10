7df5a23f279d530f51b17e97f993015a787a66b4
kizunai9 <yaodong.sun@outlook.com>
2024-06-10 21:15:29 +0200

    改了目录结构，升级了依赖

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.Static("/static", "../resources/static")
+	r.LoadHTMLGlob("../resources/templates/*.html") //加载templates目录下所有文件
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+	r.GET("/hello", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "hello.html", gin.H{})
+	})
+	r.GET("/jsTest", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "jsTest.html", gin.H{})
+	})
+	r.GET("/test1", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "test1.html", gin.H{})
+	})
+
+	err := r.Run(":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
