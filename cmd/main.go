4c77996e4768bb457ba954d865ca3017fc611f3e
kizunai9 <yaodong.sun@outlook.com>
2024-07-14 22:02:28 +0200

    API

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	//CORS策略
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:80", "http://localhost:3000"},
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept"},
+	}))
+
 	r.LoadHTMLGlob("../resources/templates/*.html")
 	r.Static("static", "../resources/static")
 
@@ -26,11 +35,28 @@ func main() {
 		c.HTML(http.StatusOK, "test1.html", gin.H{})
 	})
 
+	r.POST("/api/login", func(ctx *gin.Context) {
+		var request struct {
+			Content string `json:"content"`
+		}
+
+		if err := ctx.ShouldBindJSON(&request); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if request.Content == "hello api" {
+			ctx.JSON(http.StatusOK, gin.H{"content": "hello client!"})
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{"content": "who r u?"})
+		}
+	})
+
 	for range 3 {
 		fmt.Print("go 1.22!!!\n")
 	}
 
-	err := r.Run(":8080")
+	err := r.Run(":8000")
 	if err != nil {
 		log.Fatalln(err)
 	}
